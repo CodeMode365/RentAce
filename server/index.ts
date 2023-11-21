c588dc6f2db2e02227d3a5ce91fb06d1f118b1c2
CodeMode365 <codemode365@gmail.com>
2023-11-21 12:41:24 +0545

    adding middleware

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,15 @@
 import express, { Request, Response } from "express"
 import { prisma } from "./script"
-
+import PinRoute from "./routes/pin"
+import UserRoute from "./routes/user"
 
 async function main() {
     const PORT = process.env.PORT || 3500;
     const app = express()
+
+        .use(express.json())
+        .use("/api/pin", PinRoute)
+        .use("/api/user", UserRoute)
 
         .get("/", (req: Request, res: Response) => {
             res.send("Hello world")
