f4e3442abd8c765c57de3703b7d95644c0cae9fc
CodeMode365 <codemode365@gmail.com>
2023-11-23 22:28:36 +0545

    path change

--- client/components/Mapping/Popover.tsx
+++ client/components/Mapping/Popover.tsx
@@ -1,4 +1,4 @@
-import { IPin } from "@/lib/pins";
+import { IPin } from "@/types/pins";
 import React, { FC } from "react";
 import { FaStar } from "react-icons/fa6";
 import { Popup } from "react-map-gl";
