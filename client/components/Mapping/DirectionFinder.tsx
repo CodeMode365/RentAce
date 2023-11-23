342d6df5f885032dfffb114e6607cb9650940225
CodeMode365 <codemode365@gmail.com>
2023-11-23 22:27:36 +0545

    fixes

--- client/components/Mapping/DirectionFinder.tsx
+++ client/components/Mapping/DirectionFinder.tsx
@@ -3,7 +3,7 @@ import {
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
-import { IPin } from "@/lib/pins";
+import { IPin } from "@/types/pins";
 import axios from "axios";
 import React, { FC, useState } from "react";
 import toast from "react-hot-toast";
@@ -20,7 +20,7 @@ const DirectionFinder: FC<iProps> = ({ isLoading, startFindingDirection }) => {
     <>
       <Tooltip>
         <TooltipTrigger
-          className="absolute top-10 right-5 text-sm bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 rounded-md"
+          className="absolute top-14 right-3 text-sm bg-secondary text-primary-foreground hover:bg-secondary/90 h-10 px-4 py-2 rounded-full shadow-lg "
           disabled={isLoading}
           onClick={startFindingDirection}
         >
