d8fef97d3608b45182e4b9f6043c3953c3ce4d36
CodeMode365 <codemode365@gmail.com>
2023-11-23 21:42:37 +0545

    added ui components

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,13 +1,19 @@
 import Mapbox from "@/components/Mapping/Mapbox";
+import OptionBar from "@/components/OptionBar";
+import Sidebar from "@/components/Sidebar";
+import TopBar from "@/components/TopBar";
 import React, { useState } from "react";
 
 const Home = () => {
- 
-
   return (
-    <div>
-      <Mapbox />
-    </div>
+    <>
+      <TopBar />
+      <OptionBar />
+      <Sidebar />
+      <div>
+        <Mapbox />
+      </div>
+    </>
   );
 };
 
