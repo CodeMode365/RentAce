b3d9a1579fb929f4512ee0706c90dd94439397f4
CodeMode365 <codemode365@gmail.com>
2023-11-24 23:31:47 +0545

    route fixes

--- server/routes/media.ts
+++ server/routes/media.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import * as media from "../controller/media"
+import { uploadStream } from "../middleware/multer.mw";
 
 const router = Router()
 
     .get("/getAll", media.getAll)
-    .post("/upload", media.create)
-    .post("/delete", media.destroy)
+    .post("/upload", uploadStream.single("image"), media.create)
+    .post("/delete/:fileId", media.destroy)
 
 export default router
