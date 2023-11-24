899000264fedee3644b8d3621f1dceb43b9d586e
CodeMode365 <codemode365@gmail.com>
2023-11-24 15:56:48 +0545

    adding state management

--- client/components/OptionBar.tsx
+++ client/components/OptionBar.tsx
@@ -1,9 +1,21 @@
+"use client";
+
 import React from "react";
 import { ArrowUpDown, Locate, MapPin, Menu } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import AutoInput from "./AutoInput";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "@/lib/redux/store";
+import { openSidebar } from "@/lib/redux/slices/sidebar";
 
 const OptionBar = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const isSidebarOpen = useSelector((state: RootState) => state.sidebar.isOpen);
+
+  const openSideNav = () => {
+    dispatch(openSidebar());
+  };
+
   return (
     <section className="absolute top-3 left-3 z-50 bg-white pb-6 min-w-[300px] shadow-lg rounded-md">
       <div className="realative w-full min-h-[20px] flex items-center justify-center my-2">
@@ -11,6 +23,7 @@ const OptionBar = () => {
           size={"icon"}
           variant={"secondary"}
           className="absolute left-2 bg-transparent hover:bg-transparent"
+          onClick={() => openSideNav()}
         >
           <Menu size={20} />
         </Button>
@@ -22,10 +35,10 @@ const OptionBar = () => {
           <div className="grid grid-cols-7">
             <div className="col-span-1 flex flex-col items-center justify-center">
               <span className="m-2">
-                <Locate className="text-sky-500" size={20}/>
+                <Locate className="text-sky-500" size={20} />
               </span>
               <span className="m-2">
-                <MapPin className="text-rose-500" size={20}/>
+                <MapPin className="text-rose-500" size={20} />
               </span>
             </div>
 
@@ -36,7 +49,7 @@ const OptionBar = () => {
 
             <div className="col-span-1 flex flex-col items-center justify-center">
               <span>
-                <ArrowUpDown size={20} className="text-gray-600"/>
+                <ArrowUpDown size={20} className="text-gray-600" />
               </span>
             </div>
           </div>
