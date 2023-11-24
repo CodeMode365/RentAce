d0b00d5a64fb1a8e086309c5092b3baac90a6561
CodeMode365 <codemode365@gmail.com>
2023-11-24 15:54:00 +0545

    auth modal added

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,4 +1,6 @@
 import Mapbox from "@/components/Mapping/Mapbox";
+import AuthModal from "@/components/Modal/AuthModal";
+import LogoutModal from "@/components/Modal/LogoutModal";
 import OptionBar from "@/components/OptionBar";
 import Sidebar from "@/components/Sidebar";
 import TopBar from "@/components/TopBar";
@@ -10,6 +12,10 @@ const Home = () => {
       <TopBar />
       <OptionBar />
       <Sidebar />
+
+      <AuthModal />
+
+      <LogoutModal />
       <div>
         <Mapbox />
       </div>
