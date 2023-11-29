f1a896e47e04fbcecfac58d8dedc5b3dbea72227
CodeMode365 <codemode365@gmail.com>
2023-11-29 22:53:38 +0545

    imports cleanup

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,27 +1,13 @@
-"use client";
-
-// import Mapbox from "@/components/Mapping/Mapbox";
-// import AuthModal from "@/components/Modal/AuthModal";
-// import LogoutModal from "@/components/Modal/LogoutModal";
-// import OptionBar from "@/components/OptionBar";
-// import Sidebar from "@/components/Sidebar";
-// import TopBar from "@/components/TopBar";
 import dynamic from "next/dynamic";
 
-const TopBar = dynamic(() => import("@/components/TopBar"), { ssr: false });
+const TopBar = dynamic(() => import("@/components/TopBar"));
 const OptionBar = dynamic(() => import("@/components/OptionBar"), {
   ssr: false,
 });
-const Sidebar = dynamic(() => import("@/components/Sidebar"), { ssr: false });
-const AuthModal = dynamic(() => import("@/components/Modal/AuthModal"), {
-  ssr: false,
-});
-const LogoutModal = dynamic(() => import("@/components/Modal/LogoutModal"), {
-  ssr: false,
-});
-const Mapbox = dynamic(() => import("@/components/Mapping/Mapbox"), {
-  ssr: false,
-});
+const Sidebar = dynamic(() => import("@/components/Sidebar"));
+const AuthModal = dynamic(() => import("@/components/Modal/AuthModal"));
+const LogoutModal = dynamic(() => import("@/components/Modal/LogoutModal"));
+const Mapbox = dynamic(() => import("@/components/Mapping/Mapbox"));
 
 const Home = () => {
   return (
