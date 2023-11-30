a1c6e428201f2bee10b30f2665993d11f416f6b9
CodeMode365 <codemode365@gmail.com>
2023-11-30 16:02:54 +0545

    db enums added

---
+++ server/config/enums.ts
@@ -0,0 +1,19 @@
+
+enum SpaceTypeEnum {
+    PAKRING = "PARKING",
+    ROOM = "ROOM",
+    FLAT = "FLAT",
+    HOUSE = "HOUSE",
+    OTHER = "OTHER",
+}
+
+enum PayTypeEnum {
+    HOURLY = "DAILY",
+    DAILY = "WEEKLY",
+    WEEKLY = "HOURLY",
+    MONTHLY = "MONTHLY",
+    ANNUALLY = "ANNUALLY",
+}
+
+
+export { SpaceTypeEnum, PayTypeEnum }
