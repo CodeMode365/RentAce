8fec8d49873826f5b4175e2581f7845775a37db6
CodeMode365 <codemode365@gmail.com>
2023-12-01 16:24:56 +0545

    image large view

---
+++ client/components/reusables/FullImage.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import React from "react";
+import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
+
+const FullImage = ({
+  children,
+  src,
+  title,
+}: {
+  children: React.ReactNode;
+  src: string;
+  title: string;
+}) => {
+  return (
+    <Dialog>
+      <DialogTrigger>{children}</DialogTrigger>
+      <DialogContent className="max-h-[80vh] w-auto max-w-[70vw] overflow-hidden bg-transparent shadow-none border-none pt-10">
+        <Image
+          width={200}
+          height={200}
+          src={src}
+          alt={`${title} full image`}
+          className="object-contain h-full w-full"
+        />
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default FullImage;
