4c30cdf56ca386d2af9f44fb7faa4cc27936486c
CodeMode365 <codemode365@gmail.com>
2023-12-03 10:00:59 +0545

    commenting routes

--- server/routes/comments.ts
+++ server/routes/comments.ts
@@ -1,6 +1,13 @@
 import { Router } from "express";
+import { deleteComment, getAllComments, getComment, createComment, editComment } from "../controller/comment";
 
 const router = Router()
+
+    .get("/", getAllComments)
+    .get("/:id", getComment)
+    .post("/", createComment)
+    .patch("/:id", editComment)
+    .delete("/:id", deleteComment)
 
 
 export default router
