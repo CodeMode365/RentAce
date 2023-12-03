5f1217113ddb546ddaa5e1dbdfda3e904678c6af
CodeMode365 <codemode365@gmail.com>
2023-12-03 17:10:58 +0545

    uploading media hook added

--- client/hooks/useMedia.tsx
+++ client/hooks/useMedia.tsx
@@ -1,27 +1,35 @@
 import axios from "axios";
 import toast from "react-hot-toast";
+import useAuthKey from "./useAuthKey";
 
 function useMedia() {
   const uploadMedia = async (image: File | null | undefined) => {
     const formData = new FormData();
+    const token = useAuthKey();
+
     if (image) {
       formData.append("image", image);
-    }
 
-    await axios
-      .post(`${process.env.NEXT_PUBLIC_SERVER_URL}/media/upload`, formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      })
-      .then((res) => {
-        toast.success("Media Uploaded!");
-        console.log("Image uploaded:", res.data.data);
-      })
-      .catch((error) => {
-        toast.error("Failed to upload Media!");
-        console.error("Error:", error);
-      });
+      return await axios
+        .post(`${process.env.NEXT_PUBLIC_SERVER_URL}/media/upload`, formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((res) => {
+          toast.success("Media Uploaded!");
+          return res;
+        })
+        .catch((error) => {
+          toast.error("Failed to upload Media!");
+          return;
+        });
+      console.log("Ok");
+    } else {
+      toast.error("No Profile Image Provided");
+      return;
+    }
   };
   const deleteMedia = async () => {};
   const getAllMedia = async () => {};
