62ff919cb1f8b7971265c4052e825d5b665e0bdf
CodeMode365 <codemode365@gmail.com>
2023-12-03 17:09:16 +0545

    added space data type

---
+++ client/types/space.d.ts
@@ -0,0 +1,9 @@
+export interface iSpaceData {
+    title: string;
+    owner: string;
+    spaceType: string;
+    amount: string;
+    payType: string;
+    description: string;
+    images: File[];
+}
