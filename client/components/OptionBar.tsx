1df9100fa522d10be1581f8aabf2561cf5b4a01e
CodeMode365 <codemode365@gmail.com>
2023-12-06 16:52:15 +0545

    updated option bar for new dashboard ui

--- client/components/OptionBar.tsx
+++ client/components/OptionBar.tsx
@@ -4,30 +4,38 @@ import React from "react";
 import { ArrowUpDown, Locate, MapPin, Menu } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import AutoInput from "./AutoInput";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/lib/redux/store";
 import dynamic from "next/dynamic";
+import { openDashboard } from "@/lib/redux/slices/dashboard";
+import Operation from "./Operational/Operation";
 
 const Sidebar = dynamic(() => import("@/components/Sidebar"));
 
 const OptionBar = () => {
+  const dispatch = useDispatch();
   const isLoggedIn = useSelector(
     (state: RootState) => state.globalSetting.isLoggedIn
   );
 
+  // const dispatch = useDispatch();
+
+  const open = () => {
+    dispatch(openDashboard());
+  };
+
   return (
-    <section className="absolute top-3 left-3 z-50 bg-white pb-6 min-w-[300px] shadow-lg rounded-md">
+    <section className="absolute top-3 left-3 z-50 bg-white pb-6 shadow-lg rounded-md w-[320px] ">
       <div className="realative w-full min-h-[20px] flex items-center justify-center my-2">
         {isLoggedIn && (
-          <Sidebar>
-            <Button
-              size={"icon"}
-              variant={"secondary"}
-              className="absolute left-2 bg-transparent hover:bg-transparent"
-            >
-              <Menu size={20} />
-            </Button>
-          </Sidebar>
+          <Button
+            size={"icon"}
+            variant={"secondary"}
+            className="absolute left-2 bg-transparent hover:bg-transparent"
+            onClick={open}
+          >
+            <Menu size={20} />
+          </Button>
         )}
         <h2 className="font-semibold">Driving Direction</h2>
       </div>
@@ -57,8 +65,6 @@ const OptionBar = () => {
           </div>
         </form>
       </div>
-
-      <div></div>
     </section>
   );
 };
