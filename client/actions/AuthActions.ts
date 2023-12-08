d466f174ac9c5691059b4f02e15a988d151af70c
CodeMode365 <codemode365@gmail.com>
2023-12-08 13:42:50 +0545

    auth server action module

---
+++ client/actions/AuthActions.ts
@@ -0,0 +1,36 @@
+'use server'
+
+
+const endpoint = process.env.SERVER_URL
+
+interface iData {
+    username?: String;
+    email: String;
+    password: String;
+    isLogin: boolean;
+}
+
+export const Auth = async (data: iData) => {
+    const { username, email, password, isLogin } = data;
+    const path = isLogin ? "login" : "register";
+    const url = `${endpoint}/auth/${path}`;
+
+    const res = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            username,
+            email,
+            password,
+        }),
+    });
+
+    const response = await res.json()
+    if (!res.ok) {
+        throw new Error(response.message)
+    }
+
+    return response;
+}
