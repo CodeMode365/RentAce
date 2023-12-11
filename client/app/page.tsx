82097da3a10bddfc25fd83734db28939f485949a
CodeMode365 <codemode365@gmail.com>
2023-12-11 17:16:25 +0545

    clearup child components

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,11 +2,10 @@ import Operation from "@/components/Operational/Operation";
 import dynamic from "next/dynamic";
 
 const TopBar = dynamic(() => import("@/components/TopBar"));
-const OptionBar = dynamic(() => import("@/components/OptionBar"), {
+const OptionBar = dynamic(() => import("@/components/OptionBar"));
+const Mapbox = dynamic(() => import("@/components/Mapping/Mapbox"), {
   ssr: false,
 });
-const LogoutModal = dynamic(() => import("@/components/Modal/LogoutModal"));
-const Mapbox = dynamic(() => import("@/components/Mapping/Mapbox"));
 
 const Home = () => {
   return (
@@ -15,7 +14,6 @@ const Home = () => {
       <OptionBar />
 
       <Operation />
-      <LogoutModal />
       <div>
         <Mapbox />
       </div>
