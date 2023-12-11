f299a36209fe91587c63a8731ce392739934d1e1
CodeMode365 <codemode365@gmail.com>
2023-12-11 17:16:03 +0545

    deleted unused modules

--- server/routes/user.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { updatePassword, updateUserName } from "../controller/user";
-
-const router = Router()
-
-    .post("/name", updateUserName)
-    .post("/password", updatePassword)
-
-export default router
