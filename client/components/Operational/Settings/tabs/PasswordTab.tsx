0eb2c613a4bab17b070bf57ccf8c337ce77f8f5a
CodeMode365 <codemode365@gmail.com>
2023-12-13 18:17:52 +0545

    updated password settings tab

--- client/components/Operational/Settings/tabs/PasswordTab.tsx
+++ client/components/Operational/Settings/tabs/PasswordTab.tsx
@@ -7,21 +7,71 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import React from "react";
+import useSettings from "@/hooks/useSetting";
+import { error } from "console";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+
+const initial = {
+  old: "",
+  new: "",
+  retype: "",
+};
 
 const PasswordTab = () => {
+  const { updatePassword } = useSettings();
+  const [password, setPassword] = useState(initial);
+
+  const handleSubmit = async () => {
+    console.log(password);
+    if (password.new !== password.retype) {
+      toast.error("The new passwords doesn't match!");
+    } else {
+      await updatePassword(password.old, password.new)
+        .then((res) => {
+          setPassword(initial);
+        })
+        .catch((err) => toast.error(err.message));
+    }
+  };
+
   return (
     <Card>
       <CardHeader>
         <CardTitle className="">Change Password</CardTitle>
       </CardHeader>
       <CardContent>
-        <Input placeholder="Old password" className="my-4" />
-        <Input placeholder="New password" className="my-4" />
-        <Input placeholder="Confirm New Password" className="my-4" />
+        <Input
+          type="password"
+          placeholder="Old password"
+          className="my-4"
+          value={password.old}
+          onChange={(e) => setPassword({ ...password, old: e.target.value })}
+        />
+        <Input
+          placeholder="New password"
+          className="my-4"
+          value={password.new}
+          onChange={(e) => setPassword({ ...password, new: e.target.value })}
+          type="password"
+        />
+        <Input
+          placeholder="Confirm New Password"
+          className="my-4"
+          value={password.retype}
+          onChange={(e) => setPassword({ ...password, retype: e.target.value })}
+          type="password"
+        />
       </CardContent>
       <CardFooter className="w-full">
-        <Button className="ml-auto">Save Changes</Button>
+        <Button
+          className="ml-auto"
+          onClick={() => {
+            handleSubmit();
+          }}
+        >
+          Save Changes
+        </Button>
       </CardFooter>
     </Card>
   );
