deb10d374df247e91af4582c899c1c89d27db228
CodeMode365 <codemode365@gmail.com>
2023-12-15 14:54:02 +0545

    req limiter module

---
+++ server/utils/reqlimiter.util.ts
@@ -0,0 +1,15 @@
+import rateLimit from "express-rate-limit";
+
+
+const limiter = rateLimit({
+    windowMs: 5 * 1000,
+    limit: 10,
+    standardHeaders: "draft-7",
+    legacyHeaders: false,
+    message: "Too many requests!",
+    handler: (req, res, next) => {
+        res.status(429).json({ message: "To many requests,\n Please wait a while!" })
+    },
+})
+
+export { limiter }
