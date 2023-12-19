90076502c3301056ad223718b003bcd740174b85
CodeMode365 <codemode365@gmail.com>
2023-12-19 21:57:17 +0545

    get, post and delete ok

--- server/routes/message.ts
+++ server/routes/message.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { deleteMessage, editMessage, sendMessage } from "../controller/message";
+import { deleteMessage, editMessage, getMessages, sendMessage } from "../controller/message";
+import authenticateToken from "../middleware/token.mw";
+import { verifyToken } from "../utils/token.util";
 
 const router = Router()
 
-    .post('/send', sendMessage)
-    .delete("/delete/:messageId", deleteMessage)
-    .patch("/edit/:messageId", editMessage)
+    .get("/:conversationId", authenticateToken, getMessages)
+    .post('/', authenticateToken, sendMessage)
+    .delete("/:messageId", deleteMessage)
+    .patch("/:messageId", editMessage)
 
 export default router
