871b9b8ec87e62c4a031b6dc95092f1da3f212d5
CodeMode365 <codemode365@gmail.com>
2023-12-19 21:58:22 +0545

    getm delete, send 👍

--- server/controller/message.ts
+++ server/controller/message.ts
@@ -4,26 +4,50 @@ import { prisma } from "../script"
 import { io } from ".."
 
 const sendMessage = asyncHandler(async (req: Request, res: Response) => {
-    const { message, conversationId, userId } = req.body;
+    const { message, convId } = req.body;
+    const { userId } = req.params
 
-
-    const newMessage = await prisma.messages.create({
-        data: {
-            content: message,
-            receiver: conversationId,
-            sender: userId
-        },
-
+    const existConversation = await prisma.conversation.findFirst({
+        where: {
+            id: convId
+        }
     })
 
-    io.emit(`message-${conversationId}`, newMessage)
+    if (!existConversation) {
+        res.status(404).json({ message: "Conversation not found!" })
+        return
+    }
+
+    const newMessage = await prisma.message.create({
+        data: {
+            content: message,
+            senderId: userId,
+            conversationId: convId
+        }
+    })
+
+    // io.emit(`message-${receiverId}`, newMessage)
     res.status(201).json(newMessage)
 })
 
 const deleteMessage = asyncHandler(async (req: Request, res: Response) => {
-    const { messageId } = req.params
+    const { messageId, userId } = req.params
 
-    await prisma.messages.delete({
+    const findMessage = await prisma.message.findFirst({
+        where: {
+            AND: [
+                { senderId: userId },
+                { id: messageId }
+            ]
+        }
+    })
+
+    if (!findMessage) {
+        res.status(401).json({ message: "Not authorized to delete!" })
+        return
+    }
+
+    await prisma.message.delete({
         where: {
             id: messageId
         }
@@ -36,7 +60,7 @@ const editMessage = asyncHandler(async (req: Request, res: Response) => {
     const { messageId } = req.params
     const { newContent } = req.body
 
-    const updatedMessage = await prisma.messages.update({
+    const updatedMessage = await prisma.message.update({
         where: {
             id: messageId
         },
@@ -49,4 +73,35 @@ const editMessage = asyncHandler(async (req: Request, res: Response) => {
     res.send(200).json(updatedMessage)
 })
 
-export { sendMessage, deleteMessage, editMessage }
+const getMessages = asyncHandler(async (req: Request, res: Response) => {
+    const { page, limit } = req.query
+    const { userId, conversationId } = req.params
+
+    const messages = await prisma.message.findMany({
+        where: {
+            AND: [
+                {
+                    conversationId: conversationId
+                },
+                {
+                    conversation: {
+                        participates: {
+                            some: {
+                                id: userId
+                            }
+                        }
+                    }
+                }
+            ]
+        },
+        take: parseInt(limit as string),
+        skip: parseInt(page as string),
+        orderBy: {
+            createdAt: "desc"
+        }
+    })
+
+    res.status(200).json(messages)
+
+})
+export { sendMessage, deleteMessage, editMessage, getMessages }
