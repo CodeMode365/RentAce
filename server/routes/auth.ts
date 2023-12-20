b068a0672be21e994132d249135b206fd89a2a1a
CodeMode365 <codemode365@gmail.com>
2023-12-20 22:23:43 +0545

    authenticate current user route

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { login, register } from "../controller/auth";
+import { login, myInfo, register } from "../controller/auth";
+import authenticateToken from "../middleware/token.mw";
 
 const router = Router()
 
     .post("/login", login)
     .post("/register", register)
+    .get("/my-info", authenticateToken, myInfo)
 
 export default router
