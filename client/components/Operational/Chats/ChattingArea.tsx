352e687154bb5e87fbe71d7231145e956ca95152
CodeMode365 <codemode365@gmail.com>
2023-12-22 15:32:24 +0545

    adding realtime messages

--- client/components/Operational/Chats/ChattingArea.tsx
+++ client/components/Operational/Chats/ChattingArea.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { FC, useEffect, useState } from "react";
 import Message from "./Message";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
@@ -6,8 +6,46 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Send } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
+import { getMessages, sendMessage } from "@/actions/MsgActions";
+import useAuthKey from "@/hooks/useAuthKey";
+import toast from "react-hot-toast";
+import { IMessage } from "@/types/message";
+import { RootState } from "@/lib/redux/store";
+import { useSelector } from "react-redux";
 
-const ChattingArea = () => {
+interface iProps {
+  activeChatId: string;
+}
+
+const ChattingArea: FC<iProps> = ({ activeChatId }) => {
+  const token = useAuthKey();
+  const [page, setPage] = useState(1);
+  const [messages, setMessages] = useState<IMessage[]>([]);
+  const [inputMessage, setInputMessage] = useState("");
+  const { userInfo } = useSelector((state: RootState) => state.globalSetting);
+
+  const getUsersMessage = async () => {
+    await getMessages(token, activeChatId, page)
+      .then((res) => {
+        setMessages(res);
+      })
+      .catch((err) => {
+        toast.error(err.message);
+      });
+  };
+  useEffect(() => {
+    getUsersMessage();
+  }, [activeChatId]);
+
+  const sendCurrentMessage = async () => {
+    await sendMessage(token, inputMessage, activeChatId)
+      .then((res) => {
+        toast.success(res.message);
+        setInputMessage("");
+      })
+      .catch((error) => toast.error(error.message));
+  };
+
   return (
     <div className="flex flex-col h-full">
       <div className="row-span-1 rounded-md px-2 py-1 flex items-center border-b h-[10%]">
@@ -21,7 +59,7 @@ const ChattingArea = () => {
         </div>
         <div>
           <h2 className="text-lg text-gray-800 flex flex-col">
-            <span>Suman Tamang</span>
+            <span>{}</span>
             <span className="text-rose-500 text-sm">Busy</span>
           </h2>
         </div>
@@ -29,65 +67,29 @@ const ChattingArea = () => {
 
       <ScrollArea className="flex-1 overflow-hidden border py-3 px-3">
         <div className="flex justify-between flex-col">
-          <Message
-            content="Messege from the foreign accoutn"
-            senderType="Other"
-          />
-          <Message
-            content="Messege from the foreign accoutn"
-            senderType="User"
-          />
-          <Message
-            content="Messege from the foreign accoutn"
-            senderType="Other"
-          />
-          <Message
-            content="Messege from the foreign accoutn lore some long long text to be sent for the testing purpose of the chat box and the message handling feature, do you know you can check this  and if something goes wrognt its very much fine for me <h2 className='text-red-500'>Hello world</h2>"
-            senderType="User"
-          />
-          <Message
-            content="Messege from the foreign accoutn lore some long long text to be sent for the testing purpose of the chat box and the message handling feature, do you know you can check this  and if something goes wrognt its very much fine for me <h2 className='text-red-500'>Hello world</h2>"
-            senderType="User"
-          />
-          <Message
-            content="Messege from the foreign accoutn lore some long long text to be sent for the testing purpose of the chat box and the message handling feature, do you know you can check this  and if something goes wrognt its very much fine for me <h2 className='text-red-500'>Hello world</h2>"
-            senderType="User"
-          />
-          <Message
-            content="Messege from the foreign accoutn"
-            senderType="User"
-          />
-          <Message
-            content="Messege from the foreign accoutn lore some long long text to be sent for the testing purpose of the chat box and the message handling feature, do you know you can check this  and if something goes wrognt its very much fine for me <h2 className='text-red-500'>Hello world</h2>"
-            senderType="User"
-          />
-
-          <Message
-            content="Messege from the foreign accoutn"
-            senderType="User"
-          />
-          <Message
-            content="Messege from the foreign accoutn"
-            senderType="User"
-          />
-
-          <Message
-            content="Messege from the foreign accoutn"
-            senderType="User"
-          />
-          <Message
-            content="Messege from the foreign accoutn"
-            senderType="User"
-          />
+          {messages.map((msg) => (
+            <Message
+              content={msg.content}
+              senderType={userInfo?.id == msg.senderId ? "User" : "Other"}
+            />
+          ))}
         </div>
       </ScrollArea>
 
       <div className="flex items-center p-2">
-        <Input className="w-11/12 mr-1" placeholder="Type your message" />
+        <Input
+          className="w-11/12 mr-1"
+          placeholder="Type your message"
+          value={inputMessage}
+          onChange={(e) => setInputMessage(e.target.value)}
+        />
         <Button
           size={"icon"}
           variant={"secondary"}
           className="flex items-center justify-center text-sky-500"
+          onClick={() => {
+            sendCurrentMessage();
+          }}
         >
           <Send />
         </Button>
