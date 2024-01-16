862bc1373f79a920d43cf0fa23cf8d4e55d64cfa
CodeMode365 <codemode365@gmail.com>
2024-01-16 21:28:21 +0545

    image destroying fixed

--- server/controller/media.ts
+++ server/controller/media.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express"
 import asyncHandler from "express-async-handler"
 import { getMedias, createMedia, deleteOneMedia } from "../services/media.service"
+import { prisma } from "../script"
 
 const getAll = asyncHandler(async (req: Request, res: Response) => {
     const medias = await getMedias()
@@ -23,8 +24,26 @@ const create = asyncHandler(async (req: Request, res: Response) => {
 
 
 const destroy = asyncHandler(async (req: Request, res: Response) => {
-    const { fileId } = req.params;
-    const data = await deleteOneMedia(fileId)
+
+    const { fileId, id } = req.query;
+    const { userId } = req.params
+
+    const existingMedia = await prisma.image.findFirst({
+        where: {
+            id: id as string,
+            fileId: fileId as string,
+            creatorId: userId
+        }
+    })
+
+
+    if (!existingMedia) {
+        res.status(404).json({ message: `Media not found!` })
+        return
+    }
+
+    const data = await deleteOneMedia(fileId as string, id as string)
+
     res.status(200).json({ message: "Media deleted!", data })
 })
 
