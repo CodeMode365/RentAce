d3591e4fb6945653d850e474e953e7c2ce374741
CodeMode365 <codemode365@gmail.com>
2024-01-17 19:15:21 +0545

    extended routes for userInfo

--- server/routes/user.ts
+++ server/routes/user.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import authenticateToken from "../middleware/token.mw";
-import { getUser } from "../controller/user";
+import { getCurrentUser, getOtherUserInfo, getUserInfo, updateUserInfo } from "../controller/user";
 
 const router = Router()
 
-    .get("/my-self", authenticateToken, getUser)
+router.get("/:userId", getOtherUserInfo);
+router.get("/userInfo", authenticateToken, getUserInfo)
+router.patch("/update/userInfo", authenticateToken, updateUserInfo);
+// .get("/my-self", authenticateToken, getUser)
 
 export default router
