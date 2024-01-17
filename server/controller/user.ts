1ef5cbb972fd60e68ed9c655bd5d206cacbff731
CodeMode365 <codemode365@gmail.com>
2024-01-17 19:15:00 +0545

    user info extended controller

--- server/controller/user.ts
+++ server/controller/user.ts
@@ -2,10 +2,10 @@ import { Request, Response } from "express"
 import asyncHandler from "express-async-handler"
 import { prisma } from "../script"
 
-const getUser = asyncHandler(async (req: Request, res: Response) => {
+const getCurrentUser = asyncHandler(async (req: Request, res: Response) => {
     const { userId } = req.params
 
-    if (userId) {
+    if (!userId) {
         res.status(400).json({ message: "Request Error!" })
         return
     }
@@ -20,5 +20,82 @@ const getUser = asyncHandler(async (req: Request, res: Response) => {
     res.status(201).json({ message: "Data fethed!", data: user })
 })
 
+const getOtherUserInfo = asyncHandler(async (req: Request, res: Response) => {
+    const { userId } = req.params
 
-export { getUser }
+    if (!userId) {
+        res.status(400).json({ message: "Request Error!" })
+        return
+    }
+
+    const user = await prisma.user.findFirst({
+        where: {
+            id: userId
+        },
+        include: {
+            userInfo: {
+                select: {
+                    createdAt: false,
+                    updatedAt: false,
+                    Address: true,
+                    City: true,
+                    Country: true,
+                    phone: true,
+                    State: true,
+                    User: true
+                }
+            }
+        }
+    })
+})
+
+const getUserInfo = asyncHandler(async (req: Request, res: Response) => {
+    const { userId } = req.params
+
+    if (!userId) {
+        res.status(401).json({ message: "User not found!" })
+        return
+    }
+
+    const userInfo = await prisma.userInfo.findFirst({
+        where: {
+            User: {
+                id: userId
+            }
+        }
+    })
+
+    res.status(200).json({ message: "User Info Fetched!", data: userInfo })
+})
+
+const updateUserInfo = asyncHandler(async (req: Request, res: Response) => {
+    const { userId } = req.params
+    const { phone, Address, Country, State, City, Zip, isPublic, id } = req.body
+
+    if (!userId) {
+        res.status(401).json({ message: "User not found!" })
+        return
+    }
+
+    const updateUser = await prisma.userInfo.update({
+        where: {
+            id,
+            User: {
+                id: userId
+            }
+        },
+        data: {
+            phone,
+            Address,
+            City,
+            State,
+            Zip,
+            isPublic,
+            Country
+        }
+    })
+
+    res.status(200).json({ message: "User Info Updated!", data: updateUser })
+})
+
+export { getCurrentUser, getOtherUserInfo, getUserInfo, updateUserInfo }
