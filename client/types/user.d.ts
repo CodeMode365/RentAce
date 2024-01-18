c2af4dd8966a1f37bab0ca46652f5a20a0198903
CodeMode365 <codemode365@gmail.com>
2024-01-19 05:42:25 +0545

    Extended userInfo interface

--- client/types/user.d.ts
+++ client/types/user.d.ts
@@ -9,8 +9,24 @@ export interface IUser {
     messages?: Messages[];
     conversations?: Conversation[];
     notification?: Notification[];
+    userInfo?: IUserInfo;
     comments?: Comment[];
     ratings?: Rating[];
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface IUserInfo {
+    id: string;
+    Bio: string?;
+    phone: string?;
+    Address: string?;
+    Country: string?;
+    State: string?;
+    City: string?;
+    Zip: string?;
+    User: IUser?;
+    isPublic: boolean;
     createdAt: Date;
     updatedAt: Date;
 }
