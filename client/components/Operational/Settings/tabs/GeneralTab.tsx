c447fb795509a22bf78796adb1ef19e3b745ce72
CodeMode365 <codemode365@gmail.com>
2024-01-19 05:41:48 +0545

    basic user data fetching

--- client/components/Operational/Settings/tabs/GeneralTab.tsx
+++ client/components/Operational/Settings/tabs/GeneralTab.tsx
@@ -12,9 +12,47 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
 import { Textarea } from "@/components/ui/textarea";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getUserInfo } from "@/actions/UserAction";
+import { IUser, IUserInfo } from "@/types/user";
+import useAuthKey from "@/hooks/useAuthKey";
+import toast from "react-hot-toast";
+import { useSelector } from "react-redux";
+import { RootState } from "@/lib/redux/store";
 
 const GeneralTab = () => {
+  const token = useAuthKey();
+
+  const [userInfo, setUserInfo] = useState<IUserInfo>();
+  const [isSwitchOn, setIsSwitchOn] = useState<boolean>();
+  const userData: IUser | undefined = useSelector(
+    (state: RootState) => state.globalSetting.userInfo
+  );
+
+  const handleSwitchToggle = () => {
+    setIsSwitchOn(!isSwitchOn);
+  };
+
+  const fetchUserInfo = async () => {
+    await getUserInfo(token)
+      .then((res) => {
+        console.log(res.data);
+        toast.success(res.message);
+        setUserInfo(res.data);
+      })
+      .then(() => {
+        setIsSwitchOn(userInfo?.isPublic);
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error(error.message);
+      });
+  };
+
+  useEffect(() => {
+    fetchUserInfo();
+  }, []);
+
   return (
     <>
       <Card className="col-span-4 h-80">
@@ -30,7 +68,12 @@ const GeneralTab = () => {
           </div>
 
           <div className="text-center w-full mt-4 flex items-center justify-center">
-            <Switch id="public-profile" className="mr-4" />
+            <Switch
+              id="public-profile"
+              className="mr-4"
+              checked={isSwitchOn}
+              onChange={handleSwitchToggle}
+            />
             <Label htmlFor="public-profile m">Public Profile</Label>
           </div>
         </CardContent>
@@ -47,47 +90,82 @@ const GeneralTab = () => {
           <div className="grid grid-cols-2 gap-4 ">
             <div className="space-y-1 col-span-1">
               <Label htmlFor="name">Name</Label>
-              <Input id="name" placeholder="e.g. Ramesh Poudel" />
+              <Input
+                id="name"
+                placeholder="e.g. Ramesh Poudel"
+                defaultValue={userData!.username}
+                disabled
+              />
             </div>
             <div className="space-y-1 col-span-1">
               <Label htmlFor="email">Email</Label>
-              <Input id="email" placeholder="e.g. rameshpoudel@xyz.com" />
+              <Input
+                id="email"
+                placeholder="e.g. rameshpoudel@xyz.com"
+                defaultValue={userData!.email}
+                disabled
+              />
             </div>
           </div>
           <div className="grid grid-cols-2 gap-4 ">
             <div className="space-y-1 col-span-1">
               <Label htmlFor="phone">Phone no.</Label>
-              <Input id="phone" placeholder="e.g. 98xxxxxxx" />
+              <Input
+                id="phone"
+                placeholder="e.g. 98xxxxxxx"
+                defaultValue={userInfo?.phone ?? undefined}
+              />
             </div>
             <div className="space-y-1 col-span-1">
               <Label htmlFor="address">Address</Label>
-              <Input id="address" placeholder="e.g. Katmandu, Nepal" />
+              <Input
+                id="address"
+                placeholder="e.g. Katmandu, Nepal"
+                defaultValue={userInfo?.Address ?? undefined}
+              />
             </div>
           </div>
           <div className="grid grid-cols-2 gap-4 ">
             <div className="space-y-1 col-span-1">
               <Label htmlFor="country">Country</Label>
-              <Input id="country" placeholder="e.g. Nepal" />
+              <Input
+                id="country"
+                placeholder="e.g. Nepal"
+                defaultValue={userInfo?.Country ?? undefined}
+              />
             </div>
             <div className="space-y-1 col-span-1">
               <Label htmlFor="state">State</Label>
-              <Input id="state" placeholder="e.g. Bagmati" />
+              <Input
+                id="state"
+                placeholder="e.g. Bagmati"
+                defaultValue={userInfo?.State ?? undefined}
+              />
             </div>
           </div>
           <div className="grid grid-cols-2 gap-4 ">
             <div className="space-y-1 col-span-1">
               <Label htmlFor="city">City</Label>
-              <Input id="city" placeholder="e.g Kathmandu" />
+              <Input
+                id="city"
+                placeholder="e.g Kathmandu"
+                defaultValue={userInfo?.City ?? undefined}
+              />
             </div>
             <div className="space-y-1 col-span-1">
               <Label htmlFor="zip">Zip code</Label>
-              <Input id="zip" placeholder="e.g. 42600" />
+              <Input
+                id="zip"
+                placeholder="e.g. 42600"
+                defaultValue={userInfo?.Zip ?? undefined}
+              />
             </div>
           </div>
           <div className="grid grid-cols-2 gap-4 ">
             <Textarea
               className="col-span-2 min-h-[80px]"
               placeholder="Bio here"
+              defaultValue={userInfo?.Bio ?? undefined}
             />
           </div>
         </CardContent>
