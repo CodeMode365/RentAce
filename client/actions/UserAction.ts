24dc8e516afbdaf6e68a712cd66fc24efe30262a
CodeMode365 <codemode365@gmail.com>
2024-01-22 20:25:37 +0545

    basic userInfo updating

--- client/actions/UserAction.ts
+++ client/actions/UserAction.ts
@@ -2,8 +2,20 @@
 
 import { IUserInfo } from "@/types/user"
 import { revalidateTag } from "next/cache"
+import { getAuthToken } from "./AuthActions"
+import { z } from "zod"
 
 const url = `${process.env.SERVER_URL}/users` as string
+
+const userInfoSchema = z.object({
+    phone: z.string(),
+    Address: z.string(),
+    Country: z.string(),
+    State: z.string(),
+    City: z.string(),
+    Zip: z.string(),
+    // bio: z.string()
+})
 
 export const getUserInfo = async (token: string) => {
     const res = await fetch(`${url}/userInfo`, {
@@ -12,7 +24,7 @@ export const getUserInfo = async (token: string) => {
             "Content-type": "application/json",
             Authorization: `Bearer ${token}`
         },
-        cache: "no-cache",
+        cache: "default",
         next: {
             tags: ['userInfo']
         }
@@ -25,7 +37,28 @@ export const getUserInfo = async (token: string) => {
     return data
 }
 
-export const updateUserInfo = async (userInfo: IUserInfo, token: string) => {
+export const updateUserInfo = async (formData: FormData) => {
+    const { token } = await getAuthToken()
+
+    const userInfo = {
+        phone: formData.get('phone'),
+        State: formData.get('state'),
+        Address: formData.get('address'),
+        Country: formData.get('country'),
+        Zip: formData.get('zip'),
+        City: formData.get('city'),
+    }
+
+
+    const validatedFields = userInfoSchema.safeParse(userInfo)
+
+    // Return early if the form data is invalid
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+        }
+    }
+
     const res = await fetch(`${url}/update/userInfo`, {
         method: "PATCH",
         headers: {
