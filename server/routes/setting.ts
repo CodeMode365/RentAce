40fd31d53be6c0148b82db91f795c4039cd653f6
CodeMode365 <codemode365@gmail.com>
2024-02-01 22:26:34 +0545

    setting route fix

--- server/routes/setting.ts
+++ server/routes/setting.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
-import { updateNotificationSetting, updatePassword } from "../controller/setting";
+import { getNotificationSetting, updateNotificationSetting, updatePassword } from "../controller/setting";
 import authenticateToken from "../middleware/token.mw";
 import { updateGeneralInfo } from "../controller/setting";
+import { checkUser } from "../middleware";
 
 const router = Router()
 
-router.patch('/password', authenticateToken, updatePassword)
-router.patch("/general", authenticateToken, updateGeneralInfo)
-router.patch("/notifications", authenticateToken, updateNotificationSetting)
+    .get("/notificationSeetting", authenticateToken, checkUser, getNotificationSetting)
+    .patch('/password', authenticateToken, updatePassword)
+    .patch("/general", authenticateToken, updateGeneralInfo)
+    .patch("/update/notificationSetting", authenticateToken, checkUser, updateNotificationSetting)
 
 export default router
