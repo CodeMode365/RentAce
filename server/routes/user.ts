7f44bab8e9f14c317a152db8c755383f28f51efc
CodeMode365 <codemode365@gmail.com>
2024-02-01 22:27:23 +0545

    checkuser mw added

--- server/routes/user.ts
+++ server/routes/user.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import authenticateToken from "../middleware/token.mw";
 import { getCurrentUser, getOtherUserInfo, getUserInfo, updateUserInfo } from "../controller/user";
+import { authenticateToken, checkUser } from "../middleware";
 
 const router = Router()
 
-router.get("/userInfo", authenticateToken, getUserInfo)
-router.patch("/update/userInfo", authenticateToken, updateUserInfo);
-router.get("/:userId", getOtherUserInfo);
+router.get("/userInfo", authenticateToken, checkUser, getUserInfo)
+router.patch("/update/userInfo", authenticateToken, checkUser, updateUserInfo);
+router.get("/:userId", checkUser, getOtherUserInfo);
 // .get("/my-self", authenticateToken, getUser)
 
 export default router
